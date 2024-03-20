c7817c10a10b29b6db3d7dee7311012e95a5a305
Brad Ramnarinesingh <45901683+Bradramnarinesingh@users.noreply.github.com>
2024-03-20 18:22:54 -0400

    Delete javascript directory

--- javascript/cursor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", function (e) {
-    const cursor = document.querySelector(".cursor-glow");
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
-});
