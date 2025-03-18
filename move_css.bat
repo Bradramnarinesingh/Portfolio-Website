ed315f53271baab9652d21eb33258178982e7259
Brad Ramnarinesingh <bradramnarinesingh@gmail.com>
2025-03-18 15:23:42 -0400

    f

---
+++ move_css.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Moving CSS files to main folder...
+copy "Portfolio Website\public\css\components\layout.css" "Portfolio Website\public\css\layout.css"
+copy "Portfolio Website\public\css\components\animations.css" "Portfolio Website\public\css\animations.css"
+copy "Portfolio Website\public\css\components\cards.css" "Portfolio Website\public\css\cards.css"
+copy "Portfolio Website\public\css\components\header.css" "Portfolio Website\public\css\header.css"
+echo Files moved successfully! 
